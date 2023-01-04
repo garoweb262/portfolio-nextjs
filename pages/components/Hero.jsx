820d5638b940d356379f21b372e66016f9a7fdd5
garoweb262 <muhdgazzali01@gmail.com>
2023-01-04 12:30:09 +0100

    hero section completed, working with props types

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Hero = () => {
-  return <div>Hero</div>;
+const Hero = ({ heading, message }) => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      {/* Overlay */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
+      <div className="p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="text-xl py-5">{message}</p>
+        <button className="px-8 py-2 border">Contact me</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
